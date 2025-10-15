e14ad52eb764e5f9096f2237fc7af84c396b287f
Labaig-vs <javierlabaig@gmail.com>
2025-10-15 19:14:13 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1 class="text-2xl font-bold">Bienvenido a mi proyecto</h1>
     <p>Empieza a construir tu aplicación aquí.</p>
     <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+      Yo me lo compraría
     </button>
   </div>
 </template>
