6aa16204066e212099e29f6992fb1e30e05adadc
antonioclemente04 <antonio.clemente@esdmadrid.es>
2025-10-15 20:06:20 +0200

    Primer commit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold">Bienvenido a mi proyecto</h1>
+    <p>Empieza a construir tu aplicación aquí.</p>
+  </div>
+</template>
